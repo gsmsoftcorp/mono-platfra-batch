7848ab87c4f65842232dd507f4380e651ceba422
joo-chang <jooc0311@gmail.com>
2024-04-01 20:39:57 +0900

    first commit

---
+++ src/main/java/com/gsm/platfra/MonoPlatfraBatchApplication.java
@@ -0,0 +1,13 @@
+package com.gsm.platfra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoPlatfraBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonoPlatfraBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gsm/platfra/MonoPlatfraBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsm.platfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoPlatfraBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
